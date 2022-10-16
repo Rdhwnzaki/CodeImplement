e64ba95fb2462ea7b6bafa94fd3ba08f48a47596
Rdhwnzaki <zaki85.rmz@gmail.com>
2022-10-16 17:01:41 +0700

    add tugas

--- tugas3.js
+++ tugas3.js
@@ -1,5 +1,5 @@
 const divideAndSort = (deret) => {
-    let dataString = deret.toString().split('0').sort().join(' ')
+    let dataString = deret.toString().split('0').sort((a, b) => a - b).join('')
     console.log(dataString)
 }
 divideAndSort(5956560159466056)
